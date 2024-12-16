5a37db23390c5bd77781c6afa16765c8608fc943
Muharrem Efe Çayırbahçe <hi@efxlve.com>
2024-12-16 23:46:18 +0300

    Laying Out

--- LOTRConverter/LOTRConverter/ContentView.swift
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background Image
+            
+            VStack {
+                // Prancing Pony Logo
+                
+                // Currency Exchange Text
+                
+                // Conversion Section
+                HStack {
+                    // Left Conversion Section
+                    
+                    VStack {
+                        // Currency Picker
+                        
+                        HStack {
+                            // Currency Image
+                            
+                            // Currency Text
+                        }
+                        
+                        // TextField
+                    }
+                    
+                    // Equal Sign
+                    
+                    // Right Conversion Section
+                    
+                    VStack {
+                        // Currency Picker
+                        
+                        HStack {
+                            // Currency Text
+                            
+                            // Currency Image
+                        }
+                        
+                        // TextField
+                    }
+
+                }
+                
+                // Info Button
+            }
         }
-        .padding()
     }
 }
 
